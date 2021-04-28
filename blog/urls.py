e3a3a8c998887d4d41d2b993d65ce0b556818126
JOHNPAUL ADIMONYEMMA <johnpauladimonyemma@gmail.com>
2021-04-28 17:10:33 +0100

    update

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('', BlogListView.as_view(), name='home'),    
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),    
     path('post/<int:pk>/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDeleteView.as_view(), name='post_delete'),
