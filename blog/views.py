68498a0c1c1582a3c032b03ff159883964f55fe5
JOHNPAUL ADIMONYEMMA <johnpauladimonyemma@gmail.com>
2021-04-30 13:55:35 +0100

    Update

--- blog/views.py
+++ blog/views.py
@@ -2,35 +2,49 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin  # new
+from django.contrib.auth.mixins import UserPassesTestMixin  # new
 
 
 
 
 # Create your views here.
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'home.html'
 
 
-
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "post_detail.html"
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_new.html"
-    fields = ('title', 'author', 'body')
+    fields = ('title', 'body')
+    
+    def form_valid(self, Post):
+        Post.instance.author = self.request.user
+        return super().form_valid(Post)
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ['title', 'body']
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
  
-class BlogDeleteView(DeleteView):
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin,  DeleteView):
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy('home')
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
